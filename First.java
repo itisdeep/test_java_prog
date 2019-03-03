6bb80a11850ce565cacf9d9b36f5e8896f8c052c
Deependra <itisdeep@gmail.com>
2019-03-03 14:35:18 +0530

    Testing through Jenkins

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
 class First {
   public static void main(String[] arguments) {
-    System.out.println("Let's do something else using Java technology.");
+    System.out.println("Let's do something else using Java technology. Now testing CI with Jenkins");
   }
 }
